8827a5604c5992ddffb04f62bf551206e670fd46
Clement Lazzarini <clement.lazzarini71@gmail.com>
2025-12-12 13:01:49 +0100

    Initial commit: Setup Portfolio Charlene

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"], // On pointe vers ton fichier HTML racine
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
